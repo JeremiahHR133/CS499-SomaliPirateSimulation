0f7bc50b86e47f412c1dcebc6c703b4f9bb348c8
Jeremiah Rhoton <JeremiahHR133@gmail.com>
2023-09-05 22:54:42 -0500

    Add initial p5 structure

---
+++ src/js/simulation/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    // put setup code here
+    createCanvas(400, 400, document.getElementById("P5-DRAWING-CANVAS"));
+    background(color(0, 0, 0));
+}
+
+function draw() {
+    // put drawing code here
+}
